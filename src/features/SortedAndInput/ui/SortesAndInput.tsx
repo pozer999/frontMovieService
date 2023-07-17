5bbf9fcefe81f4f00821198378e016befa6823d1
Mihail <mishca29954@mail.ru>
2023-07-18 00:10:18 +0300

    задал состояние для select

--- src/features/SortedAndInput/ui/SortesAndInput.tsx
+++ src/features/SortedAndInput/ui/SortesAndInput.tsx
@@ -1,25 +1,21 @@
 import { Button, Col, Input, Row, Select, Space } from 'antd';
-import React from 'react';
+import React, {useState} from 'react';
 import cls from "./SortesAndInput.module.scss";
 
+
 const SortesAndInput = () => {
+
+    const [valueOptions, setValueOptions] = useState<string>();
+
   return (
     <Row justify="space-between" align="middle" style={{margin: 10}}>
     <Col span={14}>
-         <Select defaultValue="By date" style={{ width: 140 }} placement="bottomLeft"
-                options={[
-                {
-                    value: 'By date',
-                    label: 'By date',
-                },
-                {
-                    value: 'By alphabet',
-                    label: 'By alphabet',
-                },
-                {
-                    value: 'By raiting',
-                    label: 'By raiting',
-                },
+         <Select defaultValue="By date" style={{ width: 140 }} placement="bottomLeft" 
+            onChange={(value) => setValueOptions(value)} 
+            options={[
+                {value: 'By date', label: 'By date'},
+                {value: 'By alphabet', label: 'By alphabet'},
+                {value: 'By raiting',label: 'By raiting'},
                 ]}
             />
     </Col>
