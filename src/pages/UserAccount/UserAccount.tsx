405aac79a2cec7a85fecdf56351cc16a49f2f1db
Mihail <mishca29954@mail.ru>
2023-07-25 13:04:18 +0300

    недоделанная карусель исории фильмов

--- src/pages/UserAccount/UserAccount.tsx
+++ src/pages/UserAccount/UserAccount.tsx
@@ -1,33 +1,61 @@
 import { Col, Layout, Row } from 'antd';
+import cls from './UserAccount.module.scss';
+import Flickity from 'react-flickity-component';
 
 const UserAccount = () => {
-	const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
+	const style: React.CSSProperties = { background: '#0092ff' };
+	const carouselItems: string[] = [
+		'../image/kartinki-mstiteli-14.jpg',
+		'../image/2.jpg',
+		'../image/3.jpeg',
+		'../image/4.jpeg',
+		'../image/kartinki-mstiteli-14.jpg',
+		'../image/2.jpg',
+		'../image/3.jpeg',
+		'../image/4.jpeg',
+		'../image/kartinki-mstiteli-14.jpg',
+		'../image/2.jpg',
+		'../image/3.jpeg',
+		'../image/4.jpeg',
+	];
+	const flickityOptions = {
+		initialIndex: 2,
+	};
 	return (
-		<Layout.Content style={{ marginTop: 50 }}>
-			<Row>
+		<Layout.Content style={{ padding: 50, width: 500, height: 200}}>
+			<Flickity
+				className={cls.carousel}
+				elementType={'div'}
+				options={flickityOptions} 
+				disableImagesLoaded={true} 
+				reloadOnUpdate 
+				static
+			>
+				{carouselItems.map((item, index) => (
+					<img
+						src={item}
+						alt=''
+						className={cls.imageCarousel}
+						key={index}
+					/>
+				))}
+			</Flickity>
+			<Row
+				justify='space-around'
+				align='middle'>
 				<Col
 					style={style}
-					span={8}>
-					Film stories
-				</Col>
-			</Row>
-			<Row>
-				<Col
-					style={style}
-					span={3}
-					offset={2}>
+					span={5}>
 					Favorites
 				</Col>
 				<Col
 					style={style}
-					span={3}
-					offset={2}>
+					span={5}>
 					Watch later
 				</Col>
 				<Col
 					style={style}
-					span={3}
-					offset={2}>
+					span={5}>
 					Settings
 				</Col>
 			</Row>
