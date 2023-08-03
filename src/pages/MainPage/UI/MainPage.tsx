5da0c4a884e07fe8f797e8f589e89765ef616d52
Михаил <mishca29954@mail.ru>
2023-08-03 12:15:22 +0300

    исправлены ошибки с ui

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
-import { MainContainer } from '../../../widgets/MainContainer/ui/MainContainer';
 import { useEffect } from 'react';
+import { MainContainer } from '../../../widgets/MainContainer/UI/MainContainer';
 const MainPage = () => {
   const filters = useSelector((state: any) => state.films.filter);
 
